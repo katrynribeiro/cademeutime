a76a0952e63fd094ed76ade47a66f4828503a776
katrynribeiro <kakarilber@hotmail.com>
2023-11-25 09:03:46 -0300

    teste 3

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -11,12 +11,12 @@ export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/registerEvent" element={<RegisterEvent />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/mapPage" element={<MapPage />} />
-        <Route path="/EventDescription" element={<EventDescription />} />
+        <Route path="cademeutime/login" element={<Login />} />
+        <Route path="cademeutime/register" element={<Register />} />
+        <Route path="cademeutime/registerEvent" element={<RegisterEvent />} />
+        <Route path="cademeutime/" element={<Home />} />
+        <Route path="cademeutime/mapPage" element={<MapPage />} />
+        <Route path="cademeutime/EventDescription" element={<EventDescription />} />
       </Routes>
     </BrowserRouter>
   );
